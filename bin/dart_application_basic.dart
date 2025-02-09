ef9ae0d552336e84511dff18ede0f1870beff47b
Hoang5357 <ht4727430@gmail.com>
2025-02-09 09:50:42 +0700

    first comit

---
+++ bin/dart_application_basic.dart
@@ -0,0 +1,12 @@
+
+
+void main() {
+  var masosinhvien = '123';
+  int tuoi = 18;
+  var hovaten = ' tran viet hoang';
+  print('Ma so sinh vien${masosinhvien}');
+  print('ho va ten ${hovaten}');
+  print('tuoi ${tuoi}');
+    print('tuoi ${tuoi}');
+}
+
