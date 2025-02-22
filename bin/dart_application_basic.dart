56583c1323746a7f0e0db41d392b2120b5661159
Hoang5357 <ht4727430@gmail.com>
2025-02-22 21:54:42 +0700

    hoangtv

--- bin/dart_application_basic.dart
+++ bin/dart_application_basic.dart
@@ -1,17 +1,23 @@
 class SinhVien {
+  String? maSinhVien;
   String? ten;
   int? tuoi;
-  SinhVien(this.ten, this.tuoi);
-  void inRaten() {
-    print('tên: $ten - $tuoi');
+  double? diemTrungBinh;
+  SinhVien(this.maSinhVien, this.ten, this.tuoi, this.diemTrungBinh);
+  void inThongTin() {
+    print(' $maSinhVien-$ten-$tuoi-$diemTrungBinh');
   }
 }
 
 void main() {
   List<SinhVien> danhSach = [];
-  SinhVien svHoang = SinhVien('hoàng', 100);
+  SinhVien svHoang = SinhVien('SV001', "tran viet hoang", 18, 7.9);
   danhSach.add(svHoang);
+  SinhVien svHai = SinhVien('SV002', 'dam duc hai', 18, 8.9);
+  danhSach.add(svHai);
+  SinhVien svPhong = SinhVien('SV003', 'nguyen thanh phong', 18, 9.6);
+  danhSach.add(svPhong);
   for (var sv in danhSach) {
-    sv.inRaten();
+    sv.inThongTin();
   }
 }
